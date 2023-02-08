dee058e688a2b9da53af619c24b0323167590eff
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-08 18:55:09 +0300

    Home 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+int sum=a*a;
+if (sum!=b)
+{
+ Console.WriteLine("Не является:"+sum);
+}
+else if (sum==b)
+{
+ Console.WriteLine("Является квадратом:"+sum);
+}
