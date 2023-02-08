618f5194ba3ec2dc3bd25783e4580dba4e6c122e
Ilmira Sitdikova <ilmira.sitdikova.1991@mail.ru>
2023-02-08 19:35:33 +0300

    Home3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+Console.Write("Введите номер четверти: ");
+ int a = int.Parse(Console.ReadLine());
+ if (a==1)
+Console.WriteLine("x>0, y>0");
+if (a==2)
+Console.WriteLine("x<0, y>0");
+if (a==3)
+Console.WriteLine("x<0, y<0");
+if (a==4)
+Console.WriteLine("x>0, y<0");
+else
+Console.WriteLine("Error!!!");
